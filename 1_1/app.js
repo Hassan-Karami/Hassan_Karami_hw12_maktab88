44a7a9e4c4bd08d21168809c498f4ab5fd3d7f09
Your Name <you@example.com>
2023-02-11 23:16:33 +0330

    first account problem solved

--- 1_1/app.js
+++ 1_1/app.js
@@ -1,38 +1,46 @@
-"use strict";
+//name: Grayson Littel
+//Username: grayson95@ethereal.email    (also works as a real inbound email address)
+//Password:	5HHNFaANaCU8MAvv6y 
+
 const nodemailer = require("nodemailer");
+async function mailer(recieverEmail) {
+  if (typeof recieverEmail != "string") {
+    console.log("invalid input type!");
+    return;
+  } else {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(recieverEmail)) {
+      console.log("invalid email format");
+    } else {
+      let testAccount = await nodemailer.createTestAccount();
+      
+      // nodemailer configuration
+      let transporter = await nodemailer.createTransport({
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass,
+        },
+      });
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+      let info = await transporter.sendMail({
+        from: testAccount.user, // sender address
+        to: `${recieverEmail}`, // list of receivers
+        subject: "Greeting", // Subject line
+        text: "welcome to node js mailer service, it's a fake smtp service an no will send to real reciever", // plain text body
+        html: `<h3><b>welcome to node js mailer service</b></h3>
+                <p>it's a fake smtp service an no will send to real reciever</p>`, // html body
+      });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    }
+  }
 }
 
-main().catch(console.error);
+mailer("h.19687.k@gmail.com").catch(console.error);
